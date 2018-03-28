bc9e7b5da952d15a8d201524bbf4610c4e5b034e
Alain-Joseph Po-Deziel <ajdeziel@n-beta.csc.uvic.ca>
2018-03-28 12:22:29 -0700

    Initial console printout

--- a3/diskinfo.cpp
+++ a3/diskinfo.cpp
@@ -14,3 +14,34 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+
+    
+    cout << "Superblock information:";
+    cout << "Block size: ";
+    cout << "Block count: ";
+    cout << "FAT size: ";
+
+    cout << "";
+
+    cout << "FAT blocks: ";
+    cout << "Root directory start: ";
+    cout << "Root directory blocks: ";
+
+    cout << "";
+
+    cout << "FAT information: ";
+    cout << "Free Blocks: ";
+    cout << "Reserved Blocks: ";
+    cout << "Allocated Blocks: ";
+
+
+
+}
